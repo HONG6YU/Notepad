3ba6f6840af1088b9ec4530c801e98db754b4127
SimonF <LaissezFu@gmail.com>
2021-01-08 17:11:03 +0800

    Nothing

--- src/main/java/p1/Notepad.java
+++ src/main/java/p1/Notepad.java
@@ -17,15 +17,18 @@ public class Notepad {
     public String applicationName = "Notepad";
     private boolean saved = true;
     FileOperation fileOperator;
+    int ln;
+    int col;
 
     Notepad() {
         frame = new Frame(500, 500, fileName + "-" + applicationName);
-        statusBar = new JLabel("||   Ln 1, Col 1", JLabel.RIGHT);
         notepadTextArea = new SimpleTextArea(20,50);
+        ln = notepadTextArea.textArea.getLineCount();
+        statusBar = new JLabel("||  " + applicationName, JLabel.RIGHT);
         frame.add(statusBar,BorderLayout.SOUTH);
         frame.add(new JScrollPane(notepadTextArea),BorderLayout.CENTER);
         frame.pack();
-        frame.setLocation(100, 50);
+        frame.setLocation(500, 100);
         frame.setVisible(true);
         fileOperator = new FileOperation(this);
     }
